88d69e29f7897ccadaf22c04118730fb3a271d19
yongholeeme <yongholeeme@gmail.com>
2024-05-13 14:54:25 +0900

    ♻️ remove magic numbers

--- src/SecureDice.tsx
+++ src/SecureDice.tsx
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
 import styles from './SecureDice.module.css'
 
+const 주사위_최소값 = 1
+const 주사위_최대값 = 6
+
 export function SecureDice() {
-    const [diceValue, setDiceValue] = useState(1)
+    const [diceValue, setDiceValue] = useState(주사위_최소값)
 
     const rollDice = () => {
-        setDiceValue(Math.floor(Math.random() * 6) + 1)
+        setDiceValue(Math.floor(Math.random() * 주사위_최대값) + 주사위_최소값)
     }
 
     return (
